5cda58d6fcf429f8ed6b61f0178c97ca5ca7af9d
GSinghh <gbassans@yahoo.com>
2023-10-06 14:10:21 -0700

    Created route

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,21 @@
+require('dotenv').config();
+
+
 const express = require('express');
 
+//Express App
 const app = express();
 
-app.listen(3000, () => {
-    console.log("Listening on port 3000");
+//Middleware
+app.use('/', (req, res, next) => {
+    console.log(req.path, req.method);
+})
+
+//Routes
+app.get('/', (req, res) => {
+    res.json({mssg: "Teri Ma"});
+})
+
+app.listen(process.env.PORT, () => {
+    console.log('Listening on Port', process.env.PORT);
 });
