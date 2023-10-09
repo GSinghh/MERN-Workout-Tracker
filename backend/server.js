e4005d119abfcc2169226e8b3aa02fcc713f0004
GSinghh <gbassans@yahoo.com>
2023-10-08 23:58:32 -0700

    Created router

--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,18 @@ require('dotenv').config();
 
 
 const express = require('express');
-
+const workoutRoutes = require('./routes/workout')
 //Express App
 const app = express();
 
 //Middleware
 app.use('/', (req, res, next) => {
     console.log(req.path, req.method);
+    next();
 })
 
 //Routes
-app.get('/', (req, res) => {
-    res.json({mssg: "Teri Ma"});
-})
+app.use('/api/workouts/', workoutRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log('Listening on Port', process.env.PORT);
