90a3e27bdf8b43782c5088fbd9b81fb348d0e19f
GSinghh <gbassans@yahoo.com>
2023-10-09 00:06:28 -0700

    Created CRUD methods

--- backend/routes/workout.js
+++ backend/routes/workout.js
@@ -2,8 +2,28 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', () =>{
+//Getting all workouts
+router.get('/', (req, res) =>{
+    res.json({mssg: 'GET all workouts'});
+});
 
+router.get('/:id', (req, res) => {
+    res.json({mssg: 'GET a single workout'});
+})
+
+//Create a new workout
+router.post('/', (req, res) => {
+    res.json({mssg: 'POST a new workout'});
+})
+
+//Deleting a workout
+router.delete('/:id', (req, res) =>{
+    res.json({mssg: 'DELETE a workout'});
+});
+
+//Update a workout
+router.put('/:id', (req, res) => {
+    res.json({mssg: 'Updating a workout'});
 });
 
 module.exports = router;
