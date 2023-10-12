16ff68737ad676003781832e14a2be60e0f81c7f
GSinghh <gbassans@yahoo.com>
2023-10-12 09:53:51 -0700

    Added CORS

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,15 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const workoutRoutes = require('./routes/workout')
 //Express App
 const app = express();
 
 //Middleware
 app.use(express.json());
+
+app.use(cors());
 
 app.use('/', (req, res, next) => {
     console.log(req.path, req.method);
