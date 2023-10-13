81faaa8748e16297ac329c3bb31da6baca505416
GSinghh <gbassans@yahoo.com>
2023-10-13 09:14:33 -0700

    Created Form for Adding Workout

---
+++ frontend/src/components/WorkoutForm.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+export const WorkoutForm = () => {
+
+    const [title, setTitle] = useState('');
+    const [load, setLoad] = useState('');
+    const [reps, setReps] = useState('');
+    
+    return (
+        <h1>Teri Ma</h1>
+    );
+}
