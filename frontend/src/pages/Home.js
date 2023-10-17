7e6d66b138fd08fba590ed10410cd248887681e2
GSinghh <gbassans@yahoo.com>
2023-10-17 13:09:50 -0700

    Finished Context

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,11 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { WorkoutDetails } from "../components/WorkoutDetails";
 import { WorkoutForm } from "../components/WorkoutForm";
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
 
 const Home = () => {
-    
-    const [workouts, setWorkouts] = useState(null);
+
+    const {workouts, dispatch} = useWorkoutsContext();
 
     useEffect(() => {
         const fetchWorkouts = async () => {
@@ -13,7 +14,7 @@ const Home = () => {
 
             if(response.ok)
             {
-                setWorkouts(json);
+                dispatch({type: 'SET_WORKOUT', payload: json})
             }
         }
         fetchWorkouts();
