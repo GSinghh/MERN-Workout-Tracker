5636fd98b491f22cc5ae598b9b1d99e16c734ba4
GSinghh <gbassans@yahoo.com>
2023-10-23 23:52:58 -0700

    Added Signup Functionality

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -4,11 +4,22 @@ const User = require('../models/userModel');
 
 const loginUser = async (req, res) => {
     // const {email, password} = req.body;
-    res.json({mssg: "Login User Routes"})
+    res.json({mssg: 'Login User Routes'});
 }
 
 const signupUser = async (req, res) => {
-    res.json({mssg: "Signup User Routes"})
+
+    const {email, password} = req.body;
+
+    try {
+        const user = await User.signup(email, password);
+        res.status(200).json({email, user});
+
+    } catch(error) {
+
+        res.status(400).json({error: error.message})
+    }
+
 }
 
 module.exports = { signupUser, loginUser }
