26fd780c551908b11c0025762473630fac27ac12
GSinghh <gbassans@yahoo.com>
2024-01-10 00:08:18 -0800

    exporting server.js for serverless function on vercel

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,10 @@
-require('dotenv').config();
+require("dotenv").config();
 
-
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const workoutRoutes = require('./routes/workout')
-const userRoutes = require('./routes/user')
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const workoutRoutes = require("./routes/workout");
+const userRoutes = require("./routes/user");
 //Express App
 const app = express();
 
@@ -14,24 +13,26 @@ app.use(express.json());
 
 app.use(cors());
 
-app.use('/', (req, res, next) => {
-    console.log(req.path, req.method);
-    next();
-})
+app.use("/", (req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
 
 //Routes
-app.use('/api/workouts/', workoutRoutes);
-app.use('/api/user', userRoutes);
+app.use("/api/workouts/", workoutRoutes);
+app.use("/api/user", userRoutes);
 
 //Database Connection
 //Only start the server once the database connection is established
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-        app.listen(process.env.PORT, () => {
-            console.log('Connected to DB & Listening on Port', process.env.PORT);
-        });
-    })
-    .catch((error) => {
-        console.log(error);
-    })
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("Connected to DB & Listening on Port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
+module.exports = app;
