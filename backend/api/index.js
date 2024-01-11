3d678ae61a84b4a34b7d070056e0df156d4bc81a
GSinghh <gbassans@yahoo.com>
2024-01-10 18:17:43 -0800

    new directory and server.js file for vercel deployment

---
+++ backend/api/index.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const workoutRoutes = require("./routes/workout");
+const userRoutes = require("./routes/user");
+//Express App
+const app = express();
+
+//Middleware
+app.use(express.json());
+
+app.use(cors());
+
+app.use("/", (req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+//Routes
+app.use("/api/workouts/", workoutRoutes);
+app.use("/api/user", userRoutes);
+
+//Database Connection
+//Only start the server once the database connection is established
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("Connected to DB & Listening on Port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+module.exports = app;
